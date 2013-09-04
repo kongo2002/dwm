4442d041190180e549f5c933b65833d959092738
kongo2002 <kongo2002@googlemail.com>
2013-09-04 21:38:44 +0200

    remove gimp rule

--- config.h
+++ config.h
@@ -23,7 +23,6 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
     /* class      instance    title       tags mask     isfloating  monitor */
-    { "Gimp",        NULL,       NULL,       0,            True,       -1 },
     { "Skype",       NULL,       NULL,       0,            True,       -1 },
     { "yEd",         NULL,       NULL,       0,            True,       -1 },
     { "freerapid",   NULL,       NULL,       0,            True,       -1 },
